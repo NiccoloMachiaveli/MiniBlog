753395c1a2c54a462c399893c8e4610b44e42d5d
NiccoloMachiaveli <Koroliskander@yandex.ru>
2025-12-01 22:10:02 +0400

    new endpoint /register
    add register.html with form registration and get, post method /register

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-print("=== MAIN.PY ЗАПУСТИЛСЯ ===")
-
-from fastapi import FastAPI, Request
+from fastapi import FastAPI, Request, Form
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 
@@ -17,3 +15,13 @@ def home(request: Request):
 @app.get("/test")
 def test():
     return {"status": "OK"}
+
+
+@app.get("/register", response_class=HTMLResponse)
+def register(request: Request):
+    return templates.TemplateResponse("register.html", {"request": request})
+
+
+@app.post("/register")
+def test_register(username: str = Form(...), password: str = Form(...)):
+    return {"username": username, "password": password}
