3d81d40db22347c75784b3568b1f8efacebe7c09
NiccoloMachiaveli <Koroliskander@yandex.ru>
2025-12-08 20:12:25 +0400

    connection setup
    General connection setup according to standards

--- database/database.py
+++ database/database.py
@@ -1,8 +1,16 @@
+import os
 from sqlalchemy import create_engine
+from sqlalchemy.orm import DeclarativeBase, sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///database/miniblog_db.db"
-
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+SQLALCHEMY_DATABASE_URL = f"sqlite:///{os.path.join(BASE_DIR, 'database', 'miniblog_db.db')}"
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
 )
+
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+
+class Base(DeclarativeBase):
+    pass
